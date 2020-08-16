e8d68f0dff3f18ef5fd5b5c74ddb7d2a0585ff12
Ben <benknight135@gmail.com>
2020-08-16 18:29:02 +0100

    add controls to run script

--- scripts/run.bat
+++ scripts/run.bat
@@ -6,4 +6,15 @@ SET scriptpath=%~dp0
 cd %scriptpath:~0,-1%
 
 set name=helloworld
+
+echo EMULATOR CONTROLS
+echo A: B button
+echo S: A button
+echo ARROW_LEFT: Joypad Left button
+echo ARROW_RIGHT: Joypad Right button
+echo ARROW_UP: Joypad Up button
+echo ARROW_DOWN: Joypad Down button
+echo Shift: Select button
+echo Enter: Start button
+
 call bgb.exe -rom %name%.gb
